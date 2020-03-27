232775651b58cd8ac48dd53c0452d4aa0f055363
ahmadulinjs <akhmadiy@3i.ai>
2020-03-27 10:05:13 +0500

    refactor port

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ export class Server {
 
   private activeSockets: string[] = [];
 
-  private readonly DEFAULT_PORT = 5000;
+  private readonly DEFAULT_PORT = process.env.PORT || 5000;
 
   constructor() {
     this.initialize();
